b9741caf7f4f33849df46259520b20a4d3bd9cd4
dhanush <dhanushkumar4211@gmail.com>
2026-05-01 22:22:15 +0530

    final deployment fix

--- src/llm/client.py
+++ src/llm/client.py
@@ -1,33 +1,22 @@
 from openai import OpenAI
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-import os
 import streamlit as st
 
-api_key = os.getenv("OPENAI_API_KEY") or st.secrets["OPENAI_API_KEY"]
 
 class LLMClient:
     def __init__(self):
-        api_key = os.getenv("OPENAI_API_KEY")
+        api_key = os.getenv("OPENAI_API_KEY") or st.secrets["OPENAI_API_KEY"]
 
         if not api_key:
-            raise ValueError("OPENAI_API_KEY not found in environment")
+            raise ValueError("OPENAI_API_KEY not found")
 
         self.client = OpenAI(api_key=api_key)
 
     def invoke(self, prompt: str) -> str:
-        try:
-            response = self.client.chat.completions.create(
-                model="gpt-4o-mini",
-                messages=[{"role": "user", "content": prompt}],
-                temperature=0
-            )
+        response = self.client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0
+        )
 
-            return response.choices[0].message.content
-
-        except Exception as e:
-            print("LLM CLIENT ERROR:", e)
-            raise
+        return response.choices[0].message.content
